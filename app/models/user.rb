05ce6291ade4974067207304679ec2b398615c33
Jin-0127 <sakurada0127@gmail.com>
2025-02-02 06:24:42 +0000

    [add]follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
-  has_many :following, through: :active_relationships, source: :followed
+  has_many :followings, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -31,11 +31,11 @@ class User < ApplicationRecord
   end
 
   def unfollow(user)
-    active_relationships.find_by(followed_id: user.id)
+    active_relationships.find_by(followed_id: user.id).destroy
   end
 
   def following?(user)
-    following.include?(user)
+    followings.include?(user)
   end
 
 end
