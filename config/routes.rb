ef57ca0b2f47236f3678c698004ff8165b6ac651
Jin-0127 <sakurada0127@gmail.com>
2025-02-04 23:51:30 +0000

    [Finish]favorite  hidoukituushin

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "home/about"=>"homes#about"
   get "search" => "searches#search"
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
-    resource :favorite, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only:[:create,:destroy]
   end
   resources :users, only: [:index,:show,:edit,:update] do
