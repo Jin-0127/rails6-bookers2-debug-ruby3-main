82c2e48c73fe5d326b6bfe789038a70dff2b0a89
Jin-0127 <sakurada0127@gmail.com>
2025-02-05 01:00:47 +0000

    [middle]comment

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -5,12 +5,10 @@ class PostCommentsController < ApplicationController
     comment = current_user.post_comments.new(post_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
   end
 
   def  destroy
     PostComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
   end
 
   private
